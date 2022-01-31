bf3a49cd152d61166bf8cdfe039669a9be07f242
汤昊坤 <tomfff@163.com>
2022-01-31 19:15:10 +0800

    11

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+use std::{
+    env::{self, Args},
+    error::Error,
+    fs,
+};
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(mut args: Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(query) => query, // todo
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(f) => f,
+            None => return Err("Didn't get a text"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
+    let lines = search(&config.query, &content);
+
+    for line in lines.iter() {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use minigrep::Config;
+use std::{env, process};
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("{}", err)
+    }
+}
